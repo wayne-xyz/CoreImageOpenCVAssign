51569910bd37d1bbbb49f67437951ab677844524
rongwei <rongwei.ji@outlook.com>
2023-10-27 00:06:32 -0500

    set for two module, create two storyboard file for module A and B. Set segue done.

---
+++ ImageLab/EntranceViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EntranceViewController.swift
+//  ImageLab
+//
+//  Created by RongWei Ji on 10/26/23.
+//  Copyright © 2023 Eric Larson. All rights reserved.
+//
+
+import UIKit
+
+class EntranceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
