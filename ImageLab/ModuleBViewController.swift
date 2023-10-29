ed93edb26b0efc3836e263456cf7a51df0ae908e
rongwei <rongwei.ji@outlook.com>
2023-10-29 14:51:01 -0500

    set up moduleb ui

---
+++ ImageLab/ModuleBViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ModuleBViewController.swift
+//  ImageLab
+//
+//  Created by RongWei Ji on 10/29/23.
+//  Copyright © 2023 Eric Larson. All rights reserved.
+//
+
+import UIKit
+
+class ModuleBViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
