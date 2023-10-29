fc0953bf1ae43cb49fea9a484a242f1d8aa8469c
rongwei <rongwei.ji@outlook.com>
2023-10-29 15:13:50 -0500

    update ui module B

--- ImageLab/ModuleBViewController.swift
+++ ImageLab/ModuleBViewController.swift
@@ -7,15 +7,68 @@
 //
 
 import UIKit
+import MetalKit
 
 class ModuleBViewController: UIViewController {
 
+    //Properties
+    var videoManager:VisionAnalgesic!=nil
+    var detector:CIDetector!=nil
+    let bridge = OpenCVBridge()
+    var fingerFlashFlag=false;
+    
+    @IBOutlet weak var cameraView: MTKView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        //setup the OpenCV, video manager
+        self.bridge.loadHaarCascade(withFilename: "nose")
+        
+        self.videoManager=VisionAnalgesic(view: self.cameraView)
+        self.videoManager.setCameraPosition(position: AVCaptureDevice.Position.back)
         // Do any additional setup after loading the view.
     }
     
+    
+    
+    func processFinger(inputImage:CIImage)->CIImage{
+        var returnImage=inputImage
+        self.bridge.setImage(returnImage, withBounds: returnImage.extent, andContext: self.videoManager.getCIContext())
+        let processFingerResult=self.bridge.processFinger()
+        returnImage=self.bridge.getImage()
+        fingerCoverDectect(coveringBoolFlag: processFingerResult)
+        return returnImage
+    }
+    
+    func fingerCoverDectect(coveringBoolFlag:Bool){
+        if coveringBoolFlag==true{  // when something is covering disable the two button
+          
+        }else{
+         
+        }
+        
+        // finger cover happen, turn on/off flash, avoid running everytime.
+        if self.bridge.coverStatus==1{
+            if(self.fingerFlashFlag==false){
+                self.videoManager.turnOnFlashwithLevel(1.0)
+                self.fingerFlashFlag=true;
+                print("flash on.")
+            }
+          
+        }else if self.bridge.coverStatus==2{
+           
+        }else{
+            if self.fingerFlashFlag{
+                self.videoManager.turnOffFlash()
+                self.fingerFlashFlag=false;
+            }
+           
+        }
+        
+    }
 
     /*
     // MARK: - Navigation
