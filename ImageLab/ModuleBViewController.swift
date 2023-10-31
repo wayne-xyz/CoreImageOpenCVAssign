96b73a8afc0ca77163abc311efd0d420bf5d3a22
rongwei <rongwei.ji@outlook.com>
2023-10-30 20:53:40 -0500

    get the bpm done accrancy is perfect

--- ImageLab/ModuleBViewController.swift
+++ ImageLab/ModuleBViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MetalKit
+import Accelerate
 
 class ModuleBViewController: UIViewController {
 
@@ -16,6 +17,8 @@ class ModuleBViewController: UIViewController {
     var detector:CIDetector!=nil
     let bridge = OpenCVBridge()
     var fingerFlashFlag=false;
+    
+    @IBOutlet weak var bpmLabel: UILabel!
     
     @IBOutlet weak var cameraView: MTKView!
     
@@ -45,7 +48,7 @@ class ModuleBViewController: UIViewController {
         let processFingerResult=self.bridge.processFinger()
         returnImage=self.bridge.getImage()
         fingerCoverDectect(coveringBoolFlag: processFingerResult)
-        print("here is the count\(self.bridge.redArray.count) and here is the last value\(String(describing: self.bridge.redArray.lastObject))");
+        //print("here is the count\(self.bridge.redArray.count) and here is the last value\(String(describing: self.bridge.redArray.lastObject))");
       
         return returnImage
     }
@@ -72,6 +75,7 @@ class ModuleBViewController: UIViewController {
             updateColorChart(inputArray: self.bridge.redArray as! [Double],type:0)// when finger is covering the chart show the data update the main color
             findPeakArray(self.bridge.redArray.lastObject as! Double) //call find function
             updateColorChart(inputArray: peaks, type:1)
+            getBPM(beatLengthArray: beatLengths)
             
             if(self.fingerFlashFlag==false){ // this is for the flash
                _ = self.videoManager.turnOnFlashwithLevel(1.0)
@@ -90,13 +94,15 @@ class ModuleBViewController: UIViewController {
     }
 
     
-    // find out the peak of the color data
+    // find out the peak of the color data, and save the each distance of beat length
     private var windowSize = 60
     private var currentWindow: [Double] = []
     var peaks: [Double] = []
     private var currentIndex = 0
     private var lastPeakindex = -1
+    private var beatLengths:[Int]=[] // array for each peak distance
     
+    // this function is to find the peaks and save them in peaks array
     func findPeakArray(_ newData:Double){
         currentWindow.append(newData)
         
@@ -107,21 +113,39 @@ class ModuleBViewController: UIViewController {
         
         if currentIndex>=windowSize{
             if let peak = currentWindow.max() ,  peak == currentWindow[windowSize/2] {
-                peaks.append(peak)
-                 // this is the peak and in the middel of
-                // 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 4 0 0 0 0 0
-                // 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 4
+                if currentIndex - lastPeakindex > 20{
+                    beatLengths.append(currentIndex-lastPeakindex) // save the beat length for caculate the BPM
+                    
+                    peaks.append(peak) // this is the peak and in the middel of the period
+                    lastPeakindex=currentIndex
+                }else{
+                    peaks.append(0)
+                }
             }else{
                 peaks.append(0)
             }
         }else{
             peaks.append(0)
         }
-        
     }
     
+    // this func is to get the BPM from the peak distance array
+    func getBPM(beatLengthArray:[Int]){
+        if beatLengthArray.count>20{ //when the array is stable
+            // get the last new ten everage distance and get the BPM
+            var movingAverage: Double = 0.0
+            let subArray=Array(beatLengthArray[beatLengthArray.count-5...beatLengthArray.count-1])
+            let doubleDataPoints = subArray.map { Double( $0 ) }
+            vDSP_meanvD(doubleDataPoints, 1, &movingAverage, vDSP_Length(5))
+            print("5 peaks: \(doubleDataPoints)")
+            bpmLabel.text="❤️BPM:\(60000/(movingAverage*19))"
+        }
+        
+    }
+    // every two frame spend 19ms from nslog
+    // theory value 60fps camera 16ms.
     
-   
+    
     
     
     /*
